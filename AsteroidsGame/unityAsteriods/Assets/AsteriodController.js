9789914ce4153eca70e9c7ef6155fa2d50d13fe7
Charlene Delceppo <charlene.delceppo@gmail.com>
2013-03-05 08:57:18 +0100

    Changed speed of the player when pressing the spacebar on keyboard

---
+++ AsteroidsGame/unityAsteriods/Assets/AsteriodController.js
@@ -0,0 +1,16 @@
+#pragma strict
+
+var rotationSpeed:float;
+
+function Start () {
+	
+	Random.Range(80,20.00);
+
+}
+
+function Update () {
+
+	transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
+	
+
+}
